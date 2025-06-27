071702b8445a0cda80bafcbf821dc90346bf57eb
Aashir Siddiqui <aashirsiddiqui217@gmail.com>
2025-06-27 06:27:18 +0500

    Post and delete funtionality added

--- src/api/PostApi.jsx
+++ src/api/PostApi.jsx
@@ -5,6 +5,14 @@ const api = axios.create({
     baseURL: "https://jsonplaceholder.typicode.com/"
 })
 
-export const postData = () => {
+export const getPost = () => {
     return api.get("/posts")
+}
+
+export const deletePost = (id) => {
+    return api.delete(`/posts/${id}`)
+}
+
+export const postData = (post) => {
+    return api.post("/posts", post)
 }
