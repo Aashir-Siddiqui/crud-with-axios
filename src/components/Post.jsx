2a2bbdf07c701b94a045b298727fce61ceb20d02
Aashir Siddiqui <aashirsiddiqui217@gmail.com>
2025-06-28 03:10:55 +0500

    Update API data with AXIOS PUT

--- src/components/Post.jsx
+++ src/components/Post.jsx
@@ -4,6 +4,7 @@ import Form from './Form';
 
 export default function Post() {
     const [posts, setPosts] = useState([]);
+    const [updatePost, setUpdatePost] = useState({});
 
     const postData = async () => {
         const res = await getPost();
@@ -32,10 +33,12 @@ export default function Post() {
         }
     }
 
+    const handleUpdatePost = (currElem) => setUpdatePost(currElem) 
+
     return (
         <section className='flex items-center justify-center flex-col p-6 bg-zinc-900 min-h-screen'>
             <section className='bg-zinc-300 rounded-xl shadow-lg p-3 hover:shadow-xl transition-shadow duration-300 border border-zinc-100'>
-                <Form posts={posts} setPosts={setPosts} />
+                <Form posts={posts} setPosts={setPosts} updatePost={updatePost} setUpdatePost={setUpdatePost} />
             </section>
             <section className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-6 p-6">
                 {posts.map((currElem) => {
@@ -49,7 +52,7 @@ export default function Post() {
                             <p className="text-lg font-medium text-zinc-800 capitalize mb-3">{title}</p>
                             <p className="text-zinc-800 text-sm mb-4">{body}</p>
                             <div className="flex gap-3">
-                                <button className="bg-zinc-900 text-white px-4 py-2 cursor-pointer rounded-md hover:bg-zinc-950 transition-colors duration-200">
+                                <button className="bg-zinc-900 text-white px-4 py-2 cursor-pointer rounded-md hover:bg-zinc-950 transition-colors duration-200"  onClick={() => { handleUpdatePost(currElem) }}>
                                     Edit
                                 </button>
                                 <button className="bg-zinc-700 text-white px-4 py-2 cursor-pointer rounded-md hover:bg-zinc-800 transition-colors duration-200" onClick={() => { handleDeletePost(id) }}>
